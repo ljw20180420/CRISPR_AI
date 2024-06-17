2751236e7e72a5b066876c03ec45d27bc869d69b
ljw <ljw2017@sjtu.edu.cn>
2024-06-17 15:05:52 +0800

    a commit to save matlab scripts

---
+++ generate_fq_ref/cpp_bash.sh
@@ -0,0 +1,3 @@
+sudo chmod -R 777 /media/ljw/f3b85364-e45d-4166-8db8-1cca425f188e/sx/SJ-LJH-raw-data-for-paper/generate_fq_ref/results
+cd /media/ljw/f3b85364-e45d-4166-8db8-1cca425f188e/sx/SJ-LJH-raw-data-for-paper/generate_fq_ref/results
+./crispr_map -THR_MAX 24
