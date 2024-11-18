76bd003bfb54031fcf2574af693ba08c0df1af16
ljw <ljw2017@sjtu.edu.cn>
2024-11-18 14:09:37 +0800

    modulerize CRISPR diffuser dynamics.py, add dynamics figure in CRISPR diffuser space

--- AI_models/CRISPR_diffuser/app.py
+++ AI_models/CRISPR_diffuser/app.py
@@ -2,9 +2,12 @@ import gradio as gr
 from diffusers import DiffusionPipeline
 import torch
 import pandas as pd
+from tempfile import mkstemp
+from PIL import Image
 from .inference import data_collector_inference
 from ..config import get_config, get_logger
 from .scheduler import scheduler
+from .dynamics import draw
 
 args = get_config(config_file="config_CRISPR_diffuser.ini")
 logger = get_logger(args)
@@ -28,17 +31,22 @@ def app(data_name=args.data_name):
             pipe.stationary_sampler1,
             pipe.stationary_sampler2
         )
-        x1ts, x2ts, ts = pipe(batch, batch_size=args.batch_size, record_path=True)
-        return pd.DataFrame(
-            {
-                "x1": torch.cat(x1ts).tolist(),
-                "x2": torch.cat(x2ts).tolist(),
-                "t": ts * args.batch_size
-            }
+        x1ts, x2ts, _ = pipe(batch, batch_size=args.batch_size, record_path=True)
+        filename = mkstemp()[1]
+        draw(x1ts, x2ts, filename, interval=120, pad=5)
+        sequence = [ref[:x10] + ref[-args.ref2len+x20:] for x10, x20 in zip(x1ts[0], x2ts[0])]
+
+        return (
+            pd.DataFrame(
+                {
+                    "sequence": sequence
+                }
+            ),
+            Image.open(filename)
         )
 
     gr.Interface(
         fn=gradio_fn,
         inputs=["text", "number"],
-        outputs=["dataframe"],
+        outputs=["dataframe", "image"],
     ).launch()
--- AI_models/CRISPR_diffuser/dynamics.py
+++ AI_models/CRISPR_diffuser/dynamics.py
@@ -4,43 +4,17 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 import os
+from typing import Tuple, List
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from .inference import data_collector_inference
-from ..config import get_config, get_logger
-from .scheduler import scheduler
-from .model import CRISPRDiffuserModel
-from .perfect_model import PerfectModel
+from ..config import get_config
+from .scheduler import CRISPRDiffuserLinearScheduler, CRISPRDiffuserCosineScheduler, CRISPRDiffuserExpScheduler, CRISPRDiffuserUniformScheduler
 from .pipeline import CRISPRDiffuserPipeline
 
-os.environ["CUDA_VISIBLE_DEVICES"]=""
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 args = get_config("config_CRISPR_diffuser.ini")
-logger = get_logger(args)
-
-logger.info("get scheduler")
-noise_scheduler = scheduler(
-    noise_scheduler=args.noise_scheduler,
-    noise_timesteps=args.noise_timesteps,
-    cosine_factor=args.cosine_factor,
-    exp_scale=args.exp_scale,
-    exp_base=args.exp_base,
-    uniform_scale=args.uniform_scale
-)
-
-logger.info("load model")
-model = CRISPRDiffuserModel.from_pretrained("/home/ljw/sdc1/CRISPR_results/CRISPR_diffuser.max.0,1,0/SX_spcas9_CRISPR_diffuser")
-# model = CRISPRDiffuserModel.from_pretrained("/home/ljw/sdc1/CRISPR_results/CRISPR_diffuser/SX_spcas9_CRISPR_diffuser/checkpoint-322")
-# model = CRISPRDiffuserModel.from_pretrained("/home/ljw/sdc1/CRISPR_results/CRISPR_diffuser.max.1,0/SX_spcas9_CRISPR_diffuser")
-# model = CRISPRDiffuserModel.from_pretrained("/home/ljw/sdc1/CRISPR_results/CRISPR_diffuser.max.0,1/SX_spcas9_CRISPR_diffuser")
-# model = PerfectModel(CRISPRDiffuserConfig())
-
-logger.info("setup pipeline")
-pipe = CRISPRDiffuserPipeline(
-    unet=model,
-    scheduler=noise_scheduler
-)
-pipe.unet.to("cpu")
 
 @torch.no_grad()
 def to_numpy(xts):
@@ -49,7 +23,7 @@ def to_numpy(xts):
     return [xt.cpu().numpy() for xt in xts]
 
 @torch.no_grad()
-def draw(x1ts, x2ts, filename, interval=120, pad=5):
+def draw(x1ts: torch.Tensor, x2ts: torch.Tensor, filename, interval=120, pad=5):
     x1ts, x2ts = to_numpy(x1ts), to_numpy(x2ts)
     fig, ax = plt.subplots()
     scat = ax.scatter(x1ts[0], x2ts[0], c="b", s=5)
@@ -67,26 +41,19 @@ def draw(x1ts, x2ts, filename, interval=120, pad=5):
     plt.close()
 
 @torch.no_grad()
-def forward(step, x10=100, x20=27, batch_size=args.batch_size):
-    x1ts, x2ts = [torch.tensor([x10] * batch_size)], [torch.tensor([x20] * batch_size)]
-    ts = [
-        noise_scheduler.step_to_time(torch.tensor([0] * batch_size)),
-        noise_scheduler.step_to_time(torch.tensor([step] * batch_size))
-    ]
-    x1t, x2t = noise_scheduler.add_noise(
-        x1ts[0],
-        x2ts[0],
-        ts[-1],
+def forward(x10: torch.Tensor, x20: torch.Tensor, step: torch.Tensor, noise_scheduler: CRISPRDiffuserLinearScheduler | CRISPRDiffuserCosineScheduler | CRISPRDiffuserExpScheduler | CRISPRDiffuserUniformScheduler, pipe: CRISPRDiffuserPipeline) -> Tuple[torch.Tensor, torch.Tensor]:
+    # add noise to x10, x20 at step 0, return x1t, x2t at step step (step is not time)
+    return noise_scheduler.add_noise(
+        x10,
+        x20,
+        noise_scheduler.step_to_time(step),
         pipe.stationary_sampler1,
         pipe.stationary_sampler2
     )
-    x1ts.append(x1t)
-    x2ts.append(x2t)
-    return x1ts, x2ts, ts
 
 @torch.no_grad()
-def reverse(x1t, x2t, t, ref, cut, step):
-    batch_size = len(t)
+def reverse(x1t: torch.Tensor, x2t: torch.Tensor, step: torch.Tensor, ref: str, cut: int, noise_scheduler: CRISPRDiffuserLinearScheduler | CRISPRDiffuserCosineScheduler | CRISPRDiffuserExpScheduler | CRISPRDiffuserUniformScheduler, pipe: CRISPRDiffuserPipeline) -> torch.Tensor:
+    batch_size = len(step)
     batch = data_collector_inference(
         [{
             "ref": ref,
@@ -100,34 +67,18 @@ def reverse(x1t, x2t, t, ref, cut, step):
         {
             "x1t": x1t.to(pipe.unet.device),
             "x2t": x2t.to(pipe.unet.device),
-            "t": t.to(pipe.unet.device)
+            "t": noise_scheduler.step_to_time(step).to(pipe.unet.device)
         },
         batch["condition"].to(pipe.unet.device).expand(batch_size, -1, -1, -1)
     )["p_theta_0_logit"]
-    breakpoint()
-    fig, ax = plt.subplots()
-    ax.imshow(
-        F.softmax(
-            p_theta_0_logit.view(batch_size, -1),
-            dim=1
-        ).mean(axis=0).view(args.ref2len + 1, args.ref1len + 1).cpu().numpy() ** args.display_scale_factor
-    )
-    fig.savefig(f"zshit/reverse_{step}.png")
-    plt.close()
+    
+    return F.softmax(
+        p_theta_0_logit.view(batch_size, -1),
+        dim=1
+    ).mean(axis=0).view(args.ref2len + 1, args.ref1len + 1) ** args.display_scale_factor
 
 @torch.no_grad()
-def call_forward_reverse():
-    ref="AAAAAAAAAAAAAAAAAAAAAAAAGACGGCAGCCTTTTGACCTCCCAACCCCCCTATAGTCAGATAGTCAAGAAGGGCATTATCTGGCTTACCTGAATCGTCCCAAGAATTTTCTTCGGTGAGCATTTGTGGAGACCCTGGGATGTAGGTTGGATTAAACTGTGATGGGTCCATCGGCGTCTTGACACAACACTAGGCTT"
-    cut=100
-    for step in range(args.noise_timesteps + 1):
-        x1ts, x2ts, ts = forward(step, x10=100, x20=27, batch_size=args.batch_size)
-        draw(x1ts, x2ts, f'zshit/forward_{step}.gif')
-        reverse(x1ts[-1], x2ts[-1], ts[-1], ref, cut, step)
-
-@torch.no_grad()
-def dynamics(batch_size=args.batch_size, epoch=1):
-    ref="AAAAAAAAAAAAAAAAAAAAAAAAGACGGCAGCCTTTTGACCTCCCAACCCCCCTATAGTCAGATAGTCAAGAAGGGCATTATCTGGCTTACCTGAATCGTCCCAAGAATTTTCTTCGGTGAGCATTTGTGGAGACCCTGGGATGTAGGTTGGATTAAACTGTGATGGGTCCATCGGCGTCTTGACACAACACTAGGCTT"
-    cut=100
+def dynamics(batch_size: int, epoch: int, ref: str, cut: int, noise_scheduler: CRISPRDiffuserLinearScheduler | CRISPRDiffuserCosineScheduler | CRISPRDiffuserExpScheduler | CRISPRDiffuserUniformScheduler, pipe: CRISPRDiffuserPipeline) -> List[torch.Tensor]:
     batch = data_collector_inference(
         [{
             "ref": ref,
@@ -139,12 +90,13 @@ def dynamics(batch_size=args.batch_size, epoch=1):
     )
     x1ts_all, x2ts_all = None, None
     for ep in range(epoch):
-        x1ts, x2ts, ts = pipe(batch, batch_size=batch_size, record_path=True)
+        x1ts, x2ts, _ = pipe(batch, batch_size=batch_size, record_path=True)
         if not x1ts_all:
             x1ts_all = x1ts
             x2ts_all = x2ts
             continue
         x1ts_all = [torch.cat([x1t_all, x1t]) for x1t_all, x1t in zip(x1ts_all, x1ts)]
         x2ts_all = [torch.cat([x2t_all, x2t]) for x2t_all, x2t in zip(x2ts_all, x2ts)]
-    draw(x1ts_all, x2ts_all, "zshit/inference.gif")
+    
+    return x1ts_all, x2ts_all
     
