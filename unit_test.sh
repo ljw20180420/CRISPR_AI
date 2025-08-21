de0a6340e3cee8bbb68b653666584fdcb87ad994
ljw <ljw2017@sjtu.edu.cn>
2025-08-21 17:01:25 +0800

    add unit_test

---
+++ unit_test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# change to the dir of the script
+cd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+train_config=AI/preprocess/train.yaml
+output_dir=/home/ljw/sdc1/CRISPR_results
+test_config=AI/preprocess/test.yaml
+
+for data_name in SX_spcas9 SX_spymac SX_ispymac
+do
+    # for pre_model in \
+    #     CRIformer:CRIformer \
+    #     inDelphi:inDelphi \
+    #     Lindel:Lindel \
+    #     DeepHF:DeepHF \
+    #     DeepHF:CNN \
+    #     DeepHF:MLP \
+    #     DeepHF:XGBoost \
+    #     DeepHF:Ridge \
+    #     CRIfuser:CRIfuser \
+    #     FOREcasT:FOREcasT
+    # do
+    for pre_model in DeepHF:Ridge
+    do
+        IFS=":" read preprocess model_type <<<${pre_model}
+        model_config=AI/preprocess/${preprocess}/${model_type}.yaml
+        # Train
+        ./run.py train --config ${train_config} --train.output_dir ${output_dir} --train.trial_name unit_test --train.num_epochs 1 --dataset.data_file AI/dataset/test.json.gz --dataset.name ${data_name} --model ${model_config}
+
+        model_path=${output_dir}/${preprocess}/${model_type}/${data_name}/unit_test
+        # Test
+        ./run.py test --config ${test_config} --test.model_path ${model_path} --test.target CrossEntropy
+        ./run.py test --config ${test_config} --test.model_path ${model_path} --test.target NonWildTypeCrossEntropy
+    done
+done
