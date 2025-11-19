10c42fc694758bcb73a118ce88c10b0d16accd8a
ljw <ljw2017@sjtu.edu.cn>
2025-11-19 13:27:35 +0800

    Add upload.sh to upload models to huggingface.

---
+++ scripts/formal/upload.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# change to the dir of the script
+cd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# change to the dir to the project
+cd ../..
+
+function title() {
+    sharps="#################################"
+    printf "\n%s\n%s\n%s\n" ${sharps} $1 ${sharps}
+}
+
+output_dir=${OUTPUT_DIR:-$HOME"/CRISPR_results"}/formal/default
+
+for data_name in SX_spcas9 SX_spymac SX_ispymac
+do
+    title ${data_name}
+    for pre_model in \
+        CRIformer:CRIformer \
+        inDelphi:inDelphi \
+        Lindel:Lindel \
+        DeepHF:DeepHF \
+        DeepHF:CNN \
+        DeepHF:MLP \
+        DeepHF:XGBoost \
+        DeepHF:SGDClassifier \
+        CRIfuser:CRIfuser \
+        FOREcasT:FOREcasT
+    do
+        title ${pre_model}
+
+        IFS=":" read preprocess model_cls <<<${pre_model}
+        model_config=AI/preprocess/${preprocess}/${model_cls}.yaml
+
+        title "upload"
+        hf upload \
+            ${preprocess}_${model_cls}_${data_name} \
+            ${output_dir}/checkpoints/${preprocess}/${model_cls}/${data_name}/default \
+            checkpoints \
+            --delete="*"
+
+        hf upload \
+            ${preprocess}_${model_cls}_${data_name} \
+            ${output_dir}/logs/${preprocess}/${model_cls}/${data_name}/default/train \
+            logs/train \
+            --delete="*"
+    done
+done
