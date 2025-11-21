c3ac3328f191c54aa1964ad52efc05108f48e12c
ljw <ljw2017@sjtu.edu.cn>
2025-11-21 17:52:47 +0800

    Build a docker for CRISPR_AI

---
+++ docker/build_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# change to the dir of the script
+cd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# change to the dir to the project
+cd ..
+
+cp .gitignore .dockerignore
+printf ".git\n.conda\ndocker\npaper\n.vscode\nscripts/formal/*\n!scripts/formal/app.sh\n" >> .dockerignore
+printf "AI/dataset/*\n!AI/dataset/test.json.gz\n!AI/dataset/inference.csv.gz\n" >> .dockerignore
+
+docker build . \
+    --build-arg HTTP_PROXY=http://10.0.2.2:1081 \
+    --build-arg HTTPS_PROXY=http://10.0.2.2:1081 \
+    -f docker/Dockerfile \
+    -t ghcr.io/ljw20180420/crispr_ai
---
+++ docker/test_docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# refer to https://github.com/moby/moby/pull/47352 for proxy issue of docker
+# refer to https://github.com/gradio-app/gradio/issues/4046 for proxy issue of gradio
+# use -t to flush stdout, refer to https://stackoverflow.com/questions/29663459/why-doesnt-python-app-print-anything-when-run-in-a-detached-docker-container
+# Both -t and -i for Ctrl-C to work as expected (stop instead of detach), refer to https://github.com/moby/moby/issues/2838#issuecomment-29205965
+docker run --rm -it \
+    -e HTTP_PROXY=http://10.0.2.2:1081 \
+    -e HTTPS_PROXY=http://10.0.2.2:1081 \
+    -e NO_PROXY="localhost, 127.0.0.1/8, ::1" \
+    -v $HF_HOME:/AI_env/hf_cache \
+    -p 7860:7860 \
+    ghcr.io/ljw20180420/crispr_ai:latest
