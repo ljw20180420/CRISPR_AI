3995b60607a726653f6a14bd52cdcb3b09ec8442
ljw <ljw2017@sjtu.edu.cn>
2025-11-24 09:59:46 +0800

    Also copy AI/dataset/dataset.yaml into docker image.

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -7,10 +7,10 @@ cd ..
 
 cp .gitignore .dockerignore
 printf ".git\n.conda\ndocker\npaper\n.vscode\nscripts/formal/*\n!scripts/formal/app.sh\n" >> .dockerignore
-printf "AI/dataset/*\n!AI/dataset/test.json.gz\n!AI/dataset/inference.csv.gz\n" >> .dockerignore
+printf "AI/dataset/*\n!AI/dataset/dataset.yaml\n!AI/dataset/test.json.gz\n!AI/dataset/inference.csv.gz\n" >> .dockerignore
 
 docker build . \
     --build-arg HTTP_PROXY=http://10.0.2.2:1081 \
     --build-arg HTTPS_PROXY=http://10.0.2.2:1081 \
     -f docker/Dockerfile \
-    -t ghcr.io/ljw20180420/crispr_ai
+    -t ghcr.io/ljw20180420/crispr_ai:latest
