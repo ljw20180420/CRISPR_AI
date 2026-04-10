d807685dd67ba21361f68fa4118c328a626a9cc4
ljw <ljw2017@sjtu.edu.cn>
2026-04-10 16:21:31 +0800

    Update the start script app.sh for gradio app.

--- scripts/formal/app.sh
+++ scripts/formal/app.sh
@@ -12,58 +12,34 @@ function title() {
 
 app_config=AI/app.yaml
 target="GreatestCommonCrossEntropy"
+maximize_target="false"
 device=${device:-"cuda"}
 owner="ljw20180420"
 
 printf "inference:\n" > ${app_config}
 for data_name in SX_spcas9 SX_spymac SX_ispymac
 do
-    for pre_model in \
-        CRIformer:CRIformer \
-        inDelphi:inDelphi \
-        Lindel:Lindel \
-        DeepHF:DeepHF \
-        DeepHF:CNN \
-        DeepHF:MLP \
-        DeepHF:XGBoost \
-        DeepHF:SGDClassifier \
-        CRIfuser:CRIfuser \
-        FOREcasT:FOREcasT
-    do
-        printf "  - inference/inference.yaml\n" >> ${app_config}
-    done
+    printf "  - inference/inference.yaml\n" >> ${app_config}
 done
 
 printf "\ntest:\n" >> ${app_config}
 for data_name in SX_spcas9 SX_spymac SX_ispymac
 do
-    for pre_model in \
-        CRIformer:CRIformer \
-        inDelphi:inDelphi \
-        Lindel:Lindel \
-        DeepHF:DeepHF \
-        DeepHF:CNN \
-        DeepHF:MLP \
-        DeepHF:XGBoost \
-        DeepHF:SGDClassifier \
-        CRIfuser:CRIfuser \
-        FOREcasT:FOREcasT
-    do
-        IFS=":" read preprocess model_cls <<<${pre_model}
-
-        printf "  - checkpoints_path: %s/%s_%s_%s\n" \
-            ${owner} ${preprocess} ${model_cls} ${data_name} \
-            >> ${app_config}
-        printf "    logs_path: %s/%s_%s_%s\n" \
-            ${owner} ${preprocess} ${model_cls} ${data_name} \
-            >> ${app_config}
-        printf "    target: %s\n" \
-            ${target} \
-            >> ${app_config}
-        printf "    overwrite:\n      train.device: %s\n" \
-            ${device} \
-            >> ${app_config}
-    done
+    printf "  - checkpoints_path: %s/CRIfuser_CRIfuser_%s\n" \
+        ${owner} ${data_name} \
+        >> ${app_config}
+    printf "    logs_path: %s/CRIfuser_CRIfuser_%s\n" \
+        ${owner} ${data_name} \
+        >> ${app_config}
+    printf "    target: %s\n" \
+        ${target} \
+        >> ${app_config}
+    printf "    maximize_target: %s\n" \
+        ${maximize_target} \
+        >> ${app_config}
+    printf "    overwrite:\n      train.device: %s\n" \
+        ${device} \
+        >> ${app_config}
 done
 
 ./run.py app --config ${app_config}
