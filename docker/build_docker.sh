da9a13dbd43e0d119688cc25725397f29130a8e5
ljw <ljw2017@sjtu.edu.cn>
2026-04-10 17:50:31 +0800

    Download genome instead of use local hg19 builds.

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -7,19 +7,6 @@ cd ..
 
 cat .gitignore docker/dockerignoretail > .dockerignore
 
-mkdir -p docker/genome
-if ! [ -f "docker/genome/hg19.2bit" ]
-then
-    ln "${GENOME}" "docker/genome/hg19.2bit"
-fi
-for bt2 in 1.bt2 2.bt2 3.bt2 4.bt2 rev.1.bt2 rev.2.bt2
-do
-    if ! [ -f "docker/genome/hg19.${bt2}" ]
-    then
-        ln "${BOWTIE2_INDEX}.${bt2}" "docker/genome/hg19.${bt2}"
-    fi
-done
-
 docker build . \
     -f docker/Dockerfile \
     -t ghcr.io/ljw20180420/crispr_ai:latest
