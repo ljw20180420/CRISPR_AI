431fd72dbbdd45605e3bb7cbb8d9541ab5ff5562
ljw <ljw2017@sjtu.edu.cn>
2026-04-25 17:01:27 +0800

    Save hyperparameters as excel.

--- paper/hyperparameter_table.py
+++ paper/hyperparameter_table.py
@@ -3,20 +3,22 @@
 import os
 import pathlib
 
+import pandas as pd
 import yaml
 
 os.chdir(pathlib.Path(__file__).resolve().parent.parent)
 
-hparams = {}
 pre_dir = pathlib.Path("AI/preprocess")
-for preprocess in os.listdir(pre_dir):
-    if not os.path.isdir(pre_dir / preprocess):
-        continue
-    for model in os.listdir(pre_dir / preprocess):
-        if not model.endswith(".yaml"):
+with pd.ExcelWriter("paper/hyperparameters.xlsx") as ew:
+    for preprocess in os.listdir(pre_dir):
+        if not os.path.isdir(pre_dir / preprocess):
             continue
-        with open(pre_dir / preprocess / model, "r") as fd:
-            hparams[model.removesuffix(".yaml")] = yaml.safe_load(fd)["init_args"]
+        for model in os.listdir(pre_dir / preprocess):
+            if not model.endswith(".yaml"):
+                continue
+            with open(pre_dir / preprocess / model, "r") as fd:
+                hparams = yaml.safe_load(fd)["init_args"]
 
-with open("paper/hyperparameters.yaml", "w") as fd:
-    yaml.safe_dump(data=hparams, stream=fd)
+            model_cls = model.removesuffix(".yaml")
+            df = pd.DataFrame({model_cls: hparams})
+            df.to_excel(ew, sheet_name=model_cls)
